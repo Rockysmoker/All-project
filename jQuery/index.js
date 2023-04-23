7b45cdc17dca36a39ec1da032105d56df27350e8
Rockysmoker <swedzioljakub@gmail.com>
2023-04-24 00:06:45 +0200

    Lesson 155

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,7 @@
-jQuery("h1").css("color", "blue");
+// In jQuary is no deffrence for selecting all for example buttons or selecting one button (both these things we can do by simply add $ sign before ithem that we want to change!)
+
+jQuery("h1").css("color", "green");
+
+// When we write console.log($("h1").css("font-size")); we can simply check font size of our h1, but when the .css has 2 properties we change the property of font.
+
+jQuery("h1").addClass("big-title");
