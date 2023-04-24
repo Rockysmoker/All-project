b14bd2254e398907201e8c7a96b3d04da4941297
Rockysmoker <swedzioljakub@icloud.com>
2023-04-24 14:27:51 +0200

    Lesson 156

--- jQuery/index.js
+++ jQuery/index.js
@@ -5,3 +5,8 @@ jQuery("h1").css("color", "green");
 // When we write console.log($("h1").css("font-size")); we can simply check font size of our h1, but when the .css has 2 properties we change the property of font.
 
 jQuery("h1").addClass("big-title");
+$("h1").text("Bye");
+$("button").html("<em>Hey</em>");
+
+// We 
+$("a").attr("href", "https://www.yahoo.com");
