d021a664ca80694cbe331d5635ddd7ef5719305d
Rockysmoker <swedzioljakub@gmail.com>
2023-04-24 23:36:28 +0200

    Lesson 158

--- JavaScript/index(DOM&jQ).js
+++ JavaScript/index(DOM&jQ).js
@@ -46,3 +46,45 @@ function respondToKey(event){
 	console.log("Key pressed!");
 }
 
+/*------------------------jQuery-----------------------------*/
+
+// In jQuary is no deffrence for selecting all for example buttons or selecting one button (both these things we can do by simply add $ sign before ithem that we want to change!)
+
+jQuery("h1").css("color", "green");
+
+// When we write console.log($("h1").css("font-size")); we can simply check font size of our h1, but when the .css has 2 properties we change the property of font.
+
+jQuery("h1").addClass("big-title");
+$("h1").text("Bye");
+$("button").html("<em>Hey</em>");
+
+// We can add using attr difrenf page to our href
+$("a").attr("href", "https://www.yahoo.com");
+
+// Below we got the function that change css color for purple by clicking on h1 element
+
+$("h1").click(function () {
+	$("h1").css("color", "purple");
+});
+
+// Here we got 2 cases:
+//First we write pure JS code without jQuery
+for (var i = 0; i < 5; i++) {
+	document.querySelectorAll("button")[i].addEventListener("click", function () {
+		document.querySelector("h1").style.color = "purple";
+	});
+}
+//Second we write the same thing but with jQuery, its a lot shorter
+$("button").click(function () {
+	$("h1").css("color", "purple");
+});
+
+//Below we got function that chaging the h1 by showing the keys that we press on keyboard 
+$(document).keypress(function (event) {
+	$("h1").text(event.key);
+});
+
+// In this case we got the function that looking for mouse movin over the h1, then change the color of h1.
+$("h1").on("mouseover", function () {
+	$("h1").css("color", "pink");
+});
--- jQuery/index.js
+++ jQuery/index.js
@@ -8,5 +8,34 @@ jQuery("h1").addClass("big-title");
 $("h1").text("Bye");
 $("button").html("<em>Hey</em>");
 
-// We 
+// We can add using attr difrenf page to our href
 $("a").attr("href", "https://www.yahoo.com");
+
+// Below we got the function that change css color for purple by clicking on h1 element
+
+$("h1").click(function () {
+	$("h1").css("color", "purple");
+});
+
+// Here we got 2 cases:
+//First we write pure JS code without jQuery
+for (var i = 0; i < 5; i++) {
+	document.querySelectorAll("button")[i].addEventListener("click", function () {
+		document.querySelector("h1").style.color = "purple";
+	});
+}
+//Second we write the same thing but with jQuery, its a lot shorter
+$("button").click(function () {
+	$("h1").css("color", "purple");
+});
+
+//Below we got function that chaging the h1 by showing the keys that we press on keyboard
+$(document).keypress(function (event) {
+	$("h1").text(event.key);
+});
+
+// In this case we got the function that looking for mouse movin over the h1, then change the color of h1.
+$("h1").on("mouseover", function () {
+	$("h1").css("color", "pink");
+});
+
