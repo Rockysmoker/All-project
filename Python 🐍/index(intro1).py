e6c763f02042cf21db672a09d90725b9f494a45e
Rockysmoker <swedzioljakub@icloud.com>
2023-05-11 12:15:39 +0200

    Day 5

--- Python 🐍/index(intro1).py
+++ Python 🐍/index(intro1).py
@@ -365,6 +365,58 @@ map [vertical - 1][horizontal - 1] = "X"
 # 🚨 Don't change the code below 👇
 print(f"{row1}\n{row2}\n{row3}")
 
-##--------------------------------------------------------------------------
+##------------------------------Rock-Paper-Scissors--------------------------
 
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print (game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer choice:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You type invalid number, you lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw")
+
+##----------------------------GO-TO-INTRO-2-----------------------------------
--- Python 🐍/test.py
+++ Python 🐍/test.py
@@ -1,23 +1,52 @@
-# 🚨 Don't change the code below 👇
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️️","⬜️️","⬜️️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
+import random
 
-#Write your code below this row 👇
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-horizontal = int(position[0])
-vertical = int(position[1])
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-map [vertical - 1][horizontal - 1] = "X"
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#Write your code below this line 👇
 
+game_images = [rock, paper, scissors]
 
-#Write your code above this row 👆
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0:
+    print("You type invalid number, you lose!")
+else:
+    print (game_images[user_choice])
 
-# 🚨 Don't change the code below 👇
-print(f"{row1}\n{row2}\n{row3}")
+computer_choice = random.randint(0, 2)
+print("Computer choice:")
+print(game_images[computer_choice])
 
-
+if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw")
