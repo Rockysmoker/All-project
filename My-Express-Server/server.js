98173e012de2e556c1e3200dbe56b72ad303b710
Rockysmoker <swedzioljakub@icloud.com>
2023-05-18 14:03:31 +0200

    18.05.23

--- My-Express-Server/server.js
+++ My-Express-Server/server.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function (req, res){
-    response.send("Hello World!")
-})
+app.get("/", function (req, res) {
+	res.send("<h1>Hello World!</h1>");
+});
 
-app.listen(3000, function (){
-    console.log("Server started at port 3000");
+app.get("/contact", function (req, res) {
+	res.send("Contact me :)");
+});
+
+app.get("/about", function (req, res) {
+	res.send("Why you look like that?");
+});
+
+app.listen(3000, function () {
+	console.log("Server started at port 3000");
 });
