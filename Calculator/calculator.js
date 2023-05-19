7960cc6b5fd446c09ec8cbe2ee3289c11cb11cea
Rockysmoker <swedzioljakub@icloud.com>
2023-05-19 21:03:29 +0200

    Evning friday work

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+	res.send("Thanks for posting that!");
 });
 
 app.listen(3000, function () {
