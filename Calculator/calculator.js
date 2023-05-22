1e03ec5786b8f19b4380987a4847fc4b1a07f7ad
Rockysmoker <swedzioljakub@icloud.com>
2023-05-22 12:23:16 +0200

    Calculator updated

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -3,14 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-	res.send("Thanks for posting that!");
+	var num1 = Number(req.body.num1);
+	var num2 = Number(req.body.num2);
+	var result = num1 + num2;
+	res.send("The result of calculation is " + result);
+});
+
+app.get("/bmicalculator", function (req, res) {
+	res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+	var weight = parseFloat(req.body.weight);
+	var height = parseFloat(req.body.height);
+	var bmi = weight / (height * height);
+
+	res.send("Your BMI is: " + bmi);
 });
 
 app.listen(3000, function () {
