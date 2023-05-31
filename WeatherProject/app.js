bb5570d6a7915d2be85854f885331cbb1d71d07c
Rockysmoker <swedzioljakub@icloud.com>
2023-05-31 17:29:45 +0200

    Town change

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get("/", function (req, res) {
 	const url =
-		"https://api.openweathermap.org/data/2.5/weather?q=London&appid=172b0f4726adc501c48d1237bd59c3b5&units=metric";
+		"https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=172b0f4726adc501c48d1237bd59c3b5&units=metric";
 
 	https.get(url, function (response) {
 		console.log(response.statusCode);
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
 			const temp = WeatherData.main.temp;
 			const description = WeatherData.weather[0].description;
 			const icon = WeatherData.weather[0].icon;
-			
+
 			res.write("<h1>The temperature in Krakow is " + temp + ".</h1>");
 			res.write("<p>The weather is currently " + description + ".<p>");
 			res.send();
