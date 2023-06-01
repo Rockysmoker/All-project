8f5d9b7286363b921b36609d2a88e68b8a7ce98a
Rockysmoker <swedzioljakub@icloud.com>
2023-06-01 14:48:48 +0200

    01.06.23

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,27 +1,39 @@
 const { log } = require("console");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-	const url =
-		"https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=172b0f4726adc501c48d1237bd59c3b5&units=metric";
-
-	https.get(url, function (response) {
-		console.log(response.statusCode);
-		response.on("data", function (data) {
-			const WeatherData = JSON.parse(data);
-			const temp = WeatherData.main.temp;
-			const description = WeatherData.weather[0].description;
-			const icon = WeatherData.weather[0].icon;
-
-			res.write("<h1>The temperature in Krakow is " + temp + ".</h1>");
-			res.write("<p>The weather is currently " + description + ".<p>");
-			res.send();
-		});
-	});
+	res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function (req, res) {
+	console.log("Post request recived!");
+});
+
+// const query = "Krakow"
+// 	const apiKey = "172b0f4726adc501c48d1237bd59c3b5";
+// 	const unit = "metric"
+// 	const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" + apiKey + "&units="+ unit;
+
+// 	https.get(url, function (response) {
+// 		console.log(response.statusCode);
+// 		response.on("data", function (data) {
+// 			const WeatherData = JSON.parse(data);
+// 			const temp = WeatherData.main.temp;
+// 			const description = WeatherData.weather[0].description;
+// 			const icon = WeatherData.weather[0].icon;
+// 			const imageURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+// 			res.write("<h1>The temperature in Krakow is " + temp + ".</h1>");
+// 			res.write("<p>The weather is currently " + description + ".<p>");
+// 			res.write("<img src =" + imageURL + ">");
+// 			res.send();
+// 		});
+// 	});
 
 app.listen(3000, function () {
 	console.log("Server is running on port 3000");
