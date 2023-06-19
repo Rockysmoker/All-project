ebe897a638e9a202aca9b746e73d44ce9858a6ee
Rockysmoker <swedzioljakub@icloud.com>
2023-06-19 10:04:59 +0200

    res.redirect

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
 	var today = new Date();
@@ -15,7 +16,12 @@ app.get("/", function (req, res) {
 	};
 	var day = today.toLocaleDateString("en-US", options);
 
-	res.render("list", { kindOfDay: day });
+	res.render("list", { kindOfDay: day, newListIthem: ithem});
+});
+
+app.post("/", function (req, res) {   // Our input is stored in this app.post
+	var ithem = req.body.newIthem;
+	res.redirect("/");
 });
 
 app.listen(3000, function () {
