6626d95e59503f3ef3741c1bb92daebd78d57496
Rockysmoker <swedzioljakub@icloud.com>
2023-06-19 08:21:59 +0200

    First ejs tample

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -3,15 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function (req, res) {
-	
-var today = new Date();
-if (today.getDay() === 6 || today.getDay() === 0){
-	res.send("Yay it's a weekend!")
-}else{
-	res.send("Boo! I have to work.")
-}
+app.set("view engine", "ejs");
 
+app.get("/", function (req, res) {
+	var today = new Date();
+	var currentDay = today.getDay();
+	var day = "";
+
+	switch (currentDay) {
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+		
+		default:
+			console.log("Error: current day i equal to: " + currentDay);
+			break;
+	}
+	res.render("list", { kindOfDay: day }); 
 });
 
 app.listen(3000, function () {
