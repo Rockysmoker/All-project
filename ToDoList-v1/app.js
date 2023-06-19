db83b6c074ad7d20fbc7cc57a02435217280ca06
Rockysmoker <swedzioljakub@icloud.com>
2023-06-19 11:54:02 +0200

    I add styles.css to project

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -7,6 +7,7 @@ let ithems = ["Buy Food", "Cook Food", "Eat Food"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 	let today = new Date();
