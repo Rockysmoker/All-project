aeb87d9fe5952b6e4032a81976e882b8d6a139e7
Rockysmoker <swedzioljakub@icloud.com>
2023-06-19 09:11:48 +0200

    Automatic date using EJS

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -7,37 +7,15 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
 	var today = new Date();
-	var currentDay = today.getDay();
-	var day = "";
 
-	switch (currentDay) {
-		case 0:
-			day = "Sunday";
-			break;
-		case 1:
-			day = "Monday";
-			break;
-		case 2:
-			day = "Tuesday";
-			break;
-		case 3:
-			day = "Wednesday";
-			break;
-		case 4:
-			day = "Thursday";
-			break;
-		case 5:
-			day = "Friday";
-			break;
-		case 6:
-			day = "Saturday";
-			break;
-		
-		default:
-			console.log("Error: current day i equal to: " + currentDay);
-			break;
-	}
-	res.render("list", { kindOfDay: day }); 
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
+	var day = today.toLocaleDateString("en-US", options);
+
+	res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
