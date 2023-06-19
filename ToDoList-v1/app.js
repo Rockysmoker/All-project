0fa2c1d23c5252783736139dd39ee293ddcd3e6a
Rockysmoker <swedzioljakub@icloud.com>
2023-06-19 11:41:29 +0200

    Add css and change var for let

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -2,25 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+let ithems = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-	var today = new Date();
+	let today = new Date();
 
-	var options = {
+	let options = {
 		weekday: "long",
 		day: "numeric",
 		month: "long",
 	};
-	var day = today.toLocaleDateString("en-US", options);
+	let day = today.toLocaleDateString("en-US", options);
 
-	res.render("list", { kindOfDay: day, newListIthem: ithem});
+	res.render("list", { kindOfDay: day, newListIthems: ithems });
 });
 
-app.post("/", function (req, res) {   // Our input is stored in this app.post
-	var ithem = req.body.newIthem;
+app.post("/", function (req, res) {
+	// Our input is stored in this app.post
+	let ithem = req.body.newIthem;
+
+	ithems.push(ithem);
+
 	res.redirect("/");
 });
 
