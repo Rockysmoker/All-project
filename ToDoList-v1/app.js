c68a9cdab0064bc9682b34eb133d832bacb21fdb
Rockysmoker <swedzioljakub@icloud.com>
2023-06-22 11:28:53 +0200

    New blog Website Challenge

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-	let day = date();
+	let day = date.getDate();
 
 	res.render("list", { listTitle: day, newListItems: items });
 });
--- ToDoList-v1/date.js
+++ ToDoList-v1/date.js
@@ -1,13 +1,21 @@
-module.exports = getDate;
+exports.getDate = function () {
 
-function getDate() {
-	let today = new Date();
+	const today = new Date();
 
-	let options = {
+	const options = {
 		weekday: "long",
 		day: "numeric",
 		month: "long",
 	};
-	let day = today.toLocaleDateString("en-US", options);
-	return day;
+	return today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function (){
+	
+	const today = new Date();
+
+	const options = {
+		weekday: "long"
+	}
+	return today.toLocaleDateString("en-US", options);
 }
