8d6579223ed6aded5bc2f7b6d733f3112affabf3
Rockysmoker <swedzioljakub@icloud.com>
2023-06-27 12:55:47 +0200

    I add new folder with components!

--- REACT/01-starting-setup/src/App.js
+++ REACT/01-starting-setup/src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>Let's get started!</h1>
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
