8308070ada2d8607ab6f13540ce933cb6a91b2b0
Rockysmoker <swedzioljakub@icloud.com>
2023-06-27 19:19:48 +0200

    Eavning changes

--- REACT/01-starting-setup/src/components/ExpenseItem.js
+++ REACT/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-	return <h2>Expense item!</h2>;
+	return (
+		<div>
+			<h2>Expense item!</h2>
+			<p>Olcia jest bardzo dużym głupolem</p>
+			<p>Kuba też jest głupolem</p>
+			<h4>Koniec histori :/</h4>
+		</div>
+	);
 }
 
 export default ExpenseItem;
