7c4313103d3795894c43b72b8ed596238236c08e
Rockysmoker <swedzioljakub@icloud.com>
2023-06-30 14:04:44 +0200

    Very small changes with date

--- REACT/01-starting-setup/src/components/ExpenseItem.js
+++ REACT/01-starting-setup/src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<div>
+				<div>Month</div>
+				<div>Year</div>
+				<div>Day</div>
+			</div>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
