bf4717e22aa0100dbd451a54e328d0346d80e436
Rockysmoker <swedzioljakub@icloud.com>
2023-07-04 12:44:50 +0200

    Expenses finish

--- REACT/01-starting-setup/src/components/Expenses.js
+++ REACT/01-starting-setup/src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
 	return (
-		<div>
+		<div className="expenses">
 			<ExpenseItem
 				title={props.items[0].title}
 				amount={props.items[0].amount}
