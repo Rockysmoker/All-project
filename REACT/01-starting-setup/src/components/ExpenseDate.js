82a697cbe9b51acf0e971f91b42612a94a275e97
Rockysmoker <swedzioljakub@icloud.com>
2023-07-04 10:56:56 +0200

    I made new component

--- REACT/01-starting-setup/src/components/ExpenseDate.js
+++ REACT/01-starting-setup/src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import './ExpenseDate.css'
+
 function ExpenseDate(props) {
 	const month = props.date.toLocaleString("en-EN", { month: "long" });
 	const year = props.date.getFullYear();
 	const day = props.date.toLocaleString("en-EN", { day: "2-digit" });
 
 	return (
-		<div>
-			<div>{day}</div>
-			<div>{month}</div>
-			<div>{year}</div>
+		<div className = "expense-date">
+			<div className = "expanse-date__day">{day}</div>
+			<div className = "expanse-date__month">{month}</div>
+			<div className = "expense-date__year">{year}</div>
 		</div>
 	);
 }
