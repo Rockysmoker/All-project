eaba1fdb54707d6cf8d36c24094253c8bb4e693e
Rockysmoker <swedzioljakub@gmail.com>
2023-07-07 00:24:38 +0200

    i will be starting making div for small device

--- REACT/React-My-Page/src/components/Navbar.jsx
+++ REACT/React-My-Page/src/components/Navbar.jsx
@@ -1,21 +1,15 @@
-import React, { useEffect, useState } from "react";
-
-import { Link, useSearchParams } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { styles } from "../styles";
-
 import { navLinks } from "../constants";
-
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-	const [active, setactive] = useState("");
+	const [active, setActive] = useState("");
 
 	return (
 		<nav
-			className={
-				"${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary"
-			}>
+			className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 			<div className="w-full flex justify-between items-center max-w-7xl mx-auto">
 				<Link
 					to="/"
@@ -23,10 +17,27 @@ const Navbar = () => {
 					onClick={() => {
 						setActive("");
 						window.scrollTo(0, 0);
-					}}
-          >
-            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          </Link>
+					}}>
+					<img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+					<p className="text-white text-[18px] font-bold cursor-pointer">
+						Jackob <span className="sm:block hidden">| a web developer.</span>
+					</p>
+				</Link>
+				<ul className="list-none hidden sm:flex flex-row gap-10">
+					{navLinks.map((link) => (
+						<li
+							key={link.id}
+							className={`${
+								active === link.title ? "text-white" : "text-secondary"
+							} hover:text-white text-[18px] font-medium cursor-pointer`}
+							onClick={() => setActive(link.title)}>
+							<a href={`#${link.id}`}>{link.title}</a>
+						</li>
+					))}
+				</ul>
+				<div>
+					
+				</div>
 			</div>
 		</nav>
 	);
