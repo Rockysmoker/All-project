013878e8e85deeb928e433cea3517f93d39c70bd
Rockysmoker <swedzioljakub@gmail.com>
2023-07-11 00:01:44 +0200

    kurwaaaaaa

--- REACT/React-My-Page/src/components/canvas/Computers.jsx
+++ REACT/React-My-Page/src/components/canvas/Computers.jsx
@@ -11,8 +11,35 @@ const Computers = () => {
 	return (
 		<mesh>
 			<hemisphereLight intensity={0.15} groundColor="black" />
+			<pointLight intensity={1} />
+			<primitive
+				object={computers.scene}
+				scale={0.75}
+				position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+			/>
 		</mesh>
 	);
 };
 
+const ComputersCanvas = () => {
+	return (
+		<Canvas
+			frameloop="demand"
+			shadows
+			camera={{ position: [20, 3, 5], fov: 25 }}
+			gl={{ preserveDrawingBuffer: true }}>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Computers />
+			</Suspense>
+
+			<Preload all />
+		</Canvas>
+	);
+};
 export default Computers;
